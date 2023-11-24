15dee40e1ad69ce984cddb0f63abb160fa05ae0d
Maddie <maddie@Maddies-Air.attlocal.net>
2023-11-23 20:44:23 -0800

    ayo we made it to thanksgiving, she WORKS

--- client/src/App.js
+++ client/src/App.js
@@ -6,13 +6,39 @@ import VideoPlayer from './components/VideoPlayer';
 import Sidebar from './components/Sidebar';
 import Notifications from './components/Notifications';
 
+const useStyles = makeStyles((theme) => ({
+  appBar: {
+    borderRadius: 15,
+    margin: '30px 100px',
+    display: 'flex',
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center',
+    width: '600px',
+    border: '2px solid black',
+
+    [theme.breakpoints.down('xs')]: {
+      width: '90%',
+    },
+  },
+  image: {
+    marginLeft: '15px',
+  },
+  wrapper: {
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+    width: '100%',
+  },
+}));
+
 const App = () => {
   const classes = useStyles();
 
   return (
     <div>
-      <AppBar position="static" color="inherit">
-        <Typography variant="h2" align="center">Video Chat</Typography>
+      <AppBar className={classes.wrapper} position="static" color="inherit">
+        <Typography className={classes.appBar} variant="h2" align="center">Video Chat</Typography>
       </AppBar>
       <VideoPlayer />
       <Sidebar>
--- client/src/Context.js
+++ client/src/Context.js
@@ -4,7 +4,7 @@ import Peer from 'simple-peer';
 
 const SocketContext = createContext();
 
-const socket = io('http://localhost:3838');
+const socket = io('http://localhost:8000');
 
 const ContextProvider = ({ children }) => {
   const [callAccepted, setCallAccepted] = useState(false);
